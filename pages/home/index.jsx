ffd48504322bee826b4ab77de6884518d7b3251a
Nikitakumarisharma <113896209+Nikitakumarisharma@users.noreply.github.com>
2025-04-07 16:54:54 +0530

    d

---
+++ pages/home/index.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import Carausal from "@/src/components/carausal"
+import CownDown from "@/src/components/Coundown"
+import About from "@/src/components/About"
+import Service from "@/src/components/Service";
+import GallerySlider from "@/src/components/GallerySlider";
+import WhyChooseUs from "@/src/components/WhyChooseUs";
+import AnotherAbout from "@/src/components/About_2";
+import Client from "@/src/components/Client";
+import Layout from "@/src/layouts/Layout";
+import ProductPdf from "@/src/components/productpdf"
+
+
+const Index = () => {
+  // #f7931e orange
+  // #7EA63F green
+  return (
+    <Layout header={1} footer={1}>
+      <ProductPdf />
+    <Carausal /> 
+    <CownDown />
+    <About />
+    <Client />
+    <Service />
+    <GallerySlider />
+    <WhyChooseUs />
+    <AnotherAbout />
+    </Layout>
+  );
+};
+export default Index;
--- pages/index.jsx
+++ pages/index.jsx
@@ -2,36 +2,9 @@
 
 import Head from 'next/head';
 
-import React from "react";
-
-// import Carausal from "@/src/components/carausal"
-// import CownDown from "@/src/components/Coundown"
-// import About from "@/src/components/About"
-// import Service from "@/src/components/Service";
-// import GallerySlider from "@/src/components/GallerySlider";
-// import WhyChooseUs from "@/src/components/WhyChooseUs";
-// import AnotherAbout from "@/src/components/About_2";
-// import Client from "@/src/components/Client";
-// import Layout from "@/src/layouts/Layout";
-// import ProductPdf from "@/src/components/productpdf"
-
-
-const Index = () => {
-  // #f7931e orange
-  // #7EA63F green
+const MaintenancePage = () => {
   return (
-    // <Layout header={1} footer={1}>
-    //   <ProductPdf />
-    // <Carausal /> 
-    // <CownDown />
-    // <About />
-    // <Client />
-    // <Service />
-    // <GallerySlider />
-    // <WhyChooseUs />
-    // <AnotherAbout />
-    // </Layout>
-<div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-green-50 to-white">
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-green-50 to-white">
       <Head>
         <title>Maintenance | Sanskar Ayush</title>
         <meta name="description" content="We're upgrading your experience" />
@@ -94,5 +67,4 @@ const Index = () => {
   );
 };
 
-
-export default Index;
+export default MaintenancePage;
